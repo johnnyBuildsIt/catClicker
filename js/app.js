a48985e15485fc0b2fe696cc5655183c0342506a
whenwhathuh <johnnypr@udel.edu>
2019-07-18 10:02:23 -0400

    fonctionally correct

--- js/app.js
+++ js/app.js
@@ -11,16 +11,36 @@ const clickerFragment = document.createDocumentFragment();
 for (cat of cats) {
 	const catDiv = document.createElement('div');
 	catDiv.classList.add('cat-complete');
+
 	const catImg = document.createElement('img');
-	catImg.src = 'img/c1.jpeg';
+	catImg.src = `img/${cat}.jpeg`;
 	catDiv.appendChild(catImg);
+
 	const buttonDiv = document.createElement('div');
 	buttonDiv.classList.add('holder');
+
+	const button = document.createElement('button');
+	button.classList.add('like-button');
+	button.innerHTML = 'Likes!'
+
+	const likeSpan = document.createElement('span')
+	likeSpan.classList.add('likes');
+	likeSpan.innerHTML = 0;
+
+	button.addEventListener('click', (function(span){
+		let count = 0;
+		return function(){
+			count++
+			span.innerHTML = count;
+		}
+	})(likeSpan))
+
+	buttonDiv.appendChild(button);
+	buttonDiv.appendChild(likeSpan);
+
 	catDiv.appendChild(buttonDiv);
 	clickerFragment.appendChild(catDiv);
 }
-
-
 
 container.appendChild(clickerFragment);
 
