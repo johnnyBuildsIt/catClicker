6e8b1cec4ce523a4a2e85a66a128040d367fc75f
whenwhathuh <johnnypr@udel.edu>
2019-07-18 10:41:54 -0400

    cat names appearing in list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
+cats = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6'];
+
 const button = document.querySelector('#like-button');
 const likeText = document.querySelector('#likes');
 let count = 0;
+
+const ul = document.getElementsByTagName('ul')[0];
+
+for(cat of cats) {
+	const li = document.createElement('li');
+	li.innerHTML = cat;
+	ul.appendChild(li);
+}
 
 button.addEventListener('click', likeFunction);
 
